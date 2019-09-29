95ab9f5357b74a073c62d0a0af2f22995d6f642d
muratvuranok <muratvuranok@hotmail.com>
2019-09-29 19:18:26 +0300

    Alıştırmalar eklendi

---
+++ Lesson1/OperatorlerOrnekler.py
@@ -0,0 +1,99 @@
+# Örnek 1) Disaridan alinan
+# iki sayının toplamiyla farkinin birbirine bolumunden kalanin sonucu kactir?
+
+sayi1   = int(input("Lütfen birinci sayiyi giriniz : "))
+sayi2   = int(input("Lütfen ikinci sayiyi giriniz : "))
+toplam  = sayi1 + sayi2
+fark    = sayi1 - sayi2
+mod     = toplam % fark
+print("Islem sonucu :", mod)
+
+
+# Örnek 2) Disaridan girilen bir sayının 10 eksiginin 20 fazlasinin
+# 2ye bolumunden kalaninin karesi kactir?
+
+
+
+sayi3 = int(input("Lütfen bir sayi giriniz :"))
+
+sonuc =  ((sayi3 - 10  + 20) % 2 ) ** 2
+print(sonuc)
+
+# Örnek 3) Disaridan girilen iki sayının karelerinin toplami
+# ile karelerinin farki toplami kactir?
+
+
+
+
+sayi4 = int(input("Lütfen birinci sayiyi giriniz :"))
+sayi5 = int(input("Lütfen ikinci sayiyi giriniz : "))
+
+
+kare1 = sayi4 * sayi4
+kare2 = sayi5 * sayi5
+
+kare3 = sayi4 **2
+kare4 = sayi5 **2
+
+toplam = kare1 + kare2
+fark = kare1 - kare2
+sonuc = toplam + fark
+print("islem sonucu :", sonuc)
+
+
+
+
+# Örnek 4) Vize notu'nun % 30'u, final notu'nun % 70'ini alıp öğrencinin not ortalamasini
+#cikartan bir uygulama yaziniz...
+
+final = float(input("Lütfen Final Notunuzu Giriniz : "))
+vize  = float(input("Lütfen Vize Notunuzu Giriniz : "))
+not_ortalamasi   = (vize * 0.30) + (final * 0.70)
+print("Not ortalamanız :", not_ortalamasi)
+
+
+
+
+# Örnek 5) Kullanıcı ilk Adını, 2. Olarak Soyadını girsin ve kullanıcıya mesaj olarak
+# isim.soyisim@hotmail.com
+
+
+
+
+
+
+
+
+ondalikli_sayi = 14.1111111111
+print(round(ondalikli_sayi,7))  # virgulden sonraki haneyi belirlemek için kullaniriz.
+
+
+isim = input("Lütfen adınız giriniz : ")
+soyisim = input("Lütfen soyadınız giriniz : ")
+
+mail = isim + "."+ soyisim+"@hotmail.com"
+
+print(mail)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Lesson1/VeriTipleri.py
@@ -0,0 +1,55 @@
+# veri tipleri
+# convert => bir veri tipini baska bir veri tipine cevirme islemi
+
+# Convert => doviz burosu
+
+sayi = input("lutfen sayi giriniz : ")
+print(type(sayi))  
+sayi = int(sayi)
+print(type(sayi))
+
+# type(T) icerisine verdiginiz degerin veri tipi teslim eder...
+
+
+# int()   : tam sayi veri tipine cevirir.
+# str()   : strin veri tipine cevirir.
+# float() : ondalikli veri tipine cevirir.
+# chr()   : verdiginiz sayilsal degerin karakter degerini teslim eder.
+# ord()   : verdiginiz karakter degerin sayisal degerini teslim eder.
+
+print(chr(65), ord('A'))
+print(chr(90), ord('Z'))
+print(chr(97), ord('a'))
+print(chr(122), ord('z'))
+
+
+sayi = 10
+print("Toplama İşlemi Sonucu : "+str(sayi))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
