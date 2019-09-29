0b8b680a99c2a76f20e6631834b88534f6de211b
muratvuranok <muratvuranok@hotmail.com>
2019-09-29 16:17:14 +0300

    Değişkenler Örnekleri

---
+++ Lesson1/Degiskenler.py
@@ -0,0 +1,67 @@
+# Degisken tanimlama kurallari
+# 1) degisken, ismi sayi ile baslayamaz
+# 2) degisken, 2 kelimeden olusamaz(ayrik)
+# 3) degisken, ismi icerisinde sadece izin verilen ozel karakterler kullanilabilir(_)
+# 4) degisken, tanimlamasi yapilirken, kesinlikle tanimli kelimeler kullanilmaz
+# 5) degisken, ismi icerisinde turkce karakter kullanmayiniz.
+
+
+
+benimAdim    = "Murat"
+benimSoyadim = "Vuranok"
+
+print(benimAdim)
+
+# int      tam sayi
+# string   metinsel
+# float    ondaliki sayi
+# boolean  True/False mantiksal veri tipi
+
+sayi = 5
+print("islem sonucu", sayi)
+
+print(benimAdim)
+print(benimSoyadim)
+print(benimAdim + " " + benimSoyadim)
+
+# + operatoru metinsel degerlerde birlestirme islemi yapar.
+
+print(benimAdim, benimSoyadim)
+
+
+isim     = "murat"
+soyisim  = "vuranok"
+fullname = isim + " " +soyisim
+
+print("Kullanicinin Adi Soyadi : " ,         fullname       )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
